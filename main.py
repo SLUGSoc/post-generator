a60dda2bd69f7c96ee339e5a61d9fe02763775d5
Simon Fish <si@mon.fish>
2018-05-27 16:19:01 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+def new_post():
+    return {'title': None,
+            'summary': None,
+            'image': None,
+            'image_description': None,
+            'games': [],
+            'categories': None,
+            'event': {
+                'date': None,
+                'location': None
+            }
+            }
+
+
+def write_attr(post, attr):
+    if post[attr] == None:
+        print('The post has no {} yet.'.format(attr))
+    else:
+        print('Current {}: {}'.format(attr, post[attr]))
+    if isinstance(post[attr], list):
+        print('Entering multiple items for {}.'.format(attr))
+        list_input = ''
+        while True:
+            list_input = str(input('Add an item to the list: '))
+            if list_input == 'exit':
+                break
+            post[attr].append(list_input)
+    elif isinstance(post[attr], dict):
+        print('Writing attributes for {}.'.format(attr))
+        for key in post[attr]:
+            post[attr] = write_attr(post[attr], key)
+    else:
+        post[attr] = str(input('Enter the {} of the post: '.format(attr)))
+    return post
+
+
+def print_post(post):
+    print(post)
+
+
+post = new_post()
+for key in post:
+    post = write_attr(post, key)
+print(post)
