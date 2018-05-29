a34aa7209e660c9c00723861c36f9a3db755a21e
Simon Fish <si@mon.fish>
2018-05-29 12:02:49 +0100

    Segregate into separate files, begin Twitter integration

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from flask import Flask, render_template, request, Response
-from textwrap import dedent
-app = Flask(__name__)
-
-
 def new_post():
     return {'title': None,
             'summary': None,
@@ -41,43 +36,3 @@ def write_attr(post, attr):
 
 def print_post(post):
     print(post)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def create_post_route():
-    print(request.method)
-    if request.method == 'POST':
-        post = request.form.to_dict()
-        post_header = """
-        ---
-        layout: post
-        title: %s
-        summary: %s
-        prompt: Learn More
-        image: %s
-        image_description: %s
-        games: [%s]
-        categories: %s
-        event:
-          date: %s %s
-          location: %s
-        ---""" % (
-            post['title'],
-            post['summary'],
-            post['image'],
-            post['image_description'],
-            post['games'],
-            post['categories'],
-            post['event[date]'],
-            post['event[time]'],
-            post['event[location]'])
-        post_header = dedent(post_header)
-        return Response(post_header, mimetype='text/markdown; charset=UTF-8')
-    elif request.method == 'GET':
-        return render_template('create_post.html')
-    else:
-        return 'Route undefined.'
-
-
-if __name__ == '__main__':
-    app.run()
---
+++ twitter.py
@@ -0,0 +1,22 @@
+import tweepy
+import main
+
+
+def init_tweepy():
+    CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+    ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+    ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    return tweepy.API(auth)
+
+
+def update_status(api, content):
+    api.update_status(content)
+
+
+api = init_tweepy()
+post_event(api, 'Test post.')
